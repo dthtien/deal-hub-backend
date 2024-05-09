b1dde9f5fc2636f8544c00352787cbdd80ce8745
The Tiến <tiendt2311@gmail.com>
2024-05-09 20:40:46 +1000

    issue(cors): add configuration

--- app/models/product.rb
+++ app/models/product.rb
@@ -8,4 +8,17 @@ class Product < ApplicationRecord
   validates :price, presence: true
   validates :store_product_id, presence: true
   validates :store, presence: true
+
+  def as_json(options = {})
+    super(
+      options.merge(except: %i[updated_at])
+    ).merge(store_url:)
+  end
+
+  def store_url
+    case store
+    when OFFICE_WORKS
+      "https://www.officeworks.com.au#{store_path}"
+    end
+  end
 end
