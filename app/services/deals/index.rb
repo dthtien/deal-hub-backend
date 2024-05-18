6fc31c45787569306ee25424e31186581b542f95
The Tiến <tiendt2311@gmail.com>
2024-05-18 19:55:48 +1000

    ft(deals): add index service

---
+++ app/services/deals/index.rb
@@ -0,0 +1,46 @@
+module Deals
+  class Index < ApplicationService
+    attr_reader :products
+
+    def initialize(params)
+      @params = params
+      @store = params[:store]
+      @min_price = params[:min_price]
+      @max_price = params[:max_price]
+      @categories = params[:categories]
+      @order = params[:order] || {}
+      @products = Product.none
+    end
+
+    def call
+      filter
+      order_products
+
+      self
+    end
+
+    def paginate
+      @paginate ||= Pagination.new(products, params).call
+    end
+
+    private
+
+    attr_reader :store, :min_price, :max_price, :categories, :params, :order
+
+    def filter
+      scope = Product.all
+      scope = scope.where(store:) if store.present?
+      scope = scope.where('price >= ?', min_price) if min_price.present?
+      scope = scope.where('price <= ?', max_price) if max_price.present?
+      categories.present? && scope = scope.where('categories && array[?]::varchar[]', [categories].flatten)
+
+      @products = scope
+    end
+
+    def order_products
+      @products = products.order(price: order[:price]) if order[:price].present?
+      @products = products.order(updated_at: order[:updated_at]) if order[:updated_at].present?
+    end
+
+  end
+end
---
+++ spec/services/deals/index_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Deals::Index do
+  let(:params) do
+    {
+      store: 'store',
+      min_price: 10,
+      max_price: 100,
+      categories: %w[category category2],
+      order: { price: :asc }
+    }
+  end
+  let(:service) { described_class.new(params) }
+  let!(:product1) { create(:product, price: 50, categories: ['category'], store: 'store') }
+  let!(:product2) { create(:product, price: 20, categories: ['category2'], store: 'store2') }
+  let!(:product3) { create(:product, price: 30, categories: ['category3'], store: 'store') }
+  let!(:product4) { create(:product, price: 140, categories: ['category'], store: 'store') }
+  let!(:product5) { create(:product, price: 99, categories: ['category'], store: 'store') }
+
+  before { service.call }
+  context '#call' do
+    it do
+      expect(service.products).to eq([product1, product5])
+    end
+  end
+
+  context '#paginate' do
+    it do
+      expect(service.paginate).to be_a(Pagination)
+      expect(service.paginate.collection).to eq([product1, product5])
+      expect(service.paginate.metadata).to eq(
+        page: 1,
+        per_page: 25,
+        total_count: 2,
+        total_pages: 1
+      )
+    end
+  end
+end
