e8fe7e24f5d9b71721219098e06e6869cb75380e
The Tiến <tiendt2311@gmail.com>
2024-05-22 17:36:17 +1000

    ft(cors): only allow proper host

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,9 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins 'localhost:5173', 'deal-hub-fe.vercel.app', 'localhost:3000'
 
-    resource "*",
+    resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
